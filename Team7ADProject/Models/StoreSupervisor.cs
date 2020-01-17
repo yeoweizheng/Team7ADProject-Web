72baad5c9b5933f38f00ac403c9cced70f150ad3
Rishika Manocha <e0397331@u.nus.edu>
2020-01-17 16:50:32 +0800

    Store Controller and Views

---
+++ Team7ADProject/Models/StoreSupervisor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Team7ADProject.Models
+{
+    public class StoreSupervisor: User
+    {
+        public StoreSupervisor() { }
+        public StoreSupervisor(string name, string username, string password)
+            : base(name, username, password, "storeSupervisor") { }
+    }
+}
