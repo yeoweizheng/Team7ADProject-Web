57c6c3db05c7843face411679037aa415c3d08f9
Xiao Min <e0481153@u.nus.edu>
2020-01-19 18:09:48 +0800

    modelsDepartment

---
+++ Team7ADProject/Models/StockList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Team7ADProject.Models
+{
+    public class StockList
+    {
+        public int StockListId { get; set; }
+
+        public virtual Stationery Stationery { get; set; }
+        public virtual ICollection<Stationery> Stationeries { get; set; }
+
+    }
+}
