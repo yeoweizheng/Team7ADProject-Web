5165c679e39426e043e304d1ac2c8e17b33a5ef8
Wei Zheng <wzyeo1990@gmail.com>
2020-01-20 15:10:06 +0800

    Modified department request

--- Team7ADProject/Models/DepartmentRequest.cs
+++ Team7ADProject/Models/DepartmentRequest.cs
@@ -8,21 +8,14 @@ namespace Team7ADProject.Models
     public class DepartmentRequest
     {
         public int DepartmentRequestId { get; set; }
-
         public string Status { get; set; }
-
-
         public virtual Department Department { get; set; }
-
         public virtual ICollection<StationeryRequest> StationeryRequests { get; set; }
-
         public DepartmentRequest(Department department)
         {
             this.StationeryRequests = new List<StationeryRequest>();
             this.Department = department;
             this.Status = "Not Retrieved";
-            
         }
-
     }
 }
