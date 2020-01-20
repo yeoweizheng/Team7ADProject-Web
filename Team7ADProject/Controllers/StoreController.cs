d14232a2f9ec0171656c0db63122abe339e736d6
Nus-Ge <gaogesg@gmail.com>
2020-01-20 14:39:13 +0800

    StockDetailsView

--- Team7ADProject/Controllers/StoreController.cs
+++ Team7ADProject/Controllers/StoreController.cs
@@ -73,8 +73,9 @@ namespace Team7ADProject.Controllers
             User user = HomeController.GetUserFromCookie(Request.Cookies["Team7ADProject"]);
             if (user == null) return RedirectToAction("Index", "Home");
             if (user.UserType != "storeClerk" && user.UserType != "storeSupervisor") return RedirectToAction("Index", "Home");
+            Stationery stationery = db.Stationery.Where(x=> x.StationeryId == stationeryId).FirstOrDefault();
+            ViewData["stationery"] = stationery;
             ViewData["sidenavItems"] = clerkSideNavItems;
-            System.Diagnostics.Debug.WriteLine(stationeryId + "");
             return View();
         }
 
