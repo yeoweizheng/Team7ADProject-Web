2a5144676c6b5a41bf57aeb665f07a64e3464f86
Wei Zheng <wzyeo1990@gmail.com>
2020-01-20 14:46:24 +0800

    Created notification service

--- Team7ADProject/Controllers/DepartmentController.cs
+++ Team7ADProject/Controllers/DepartmentController.cs
@@ -15,14 +15,16 @@ namespace Team7ADProject.Controllers
         private static List<SidenavItem> headSidenavItems;
         private static Team7ADProjectDbContext db;
         private static UserService userService;
+        private static NotificationService notificationService;
         public static void Init()
         {
             db = new Team7ADProjectDbContext();
             userService = new UserService();
+            notificationService = new NotificationService();
             staffSidenavItems = new List<SidenavItem>();
             staffSidenavItems.Add(new SidenavItem("Stationery Requests", "/Department/StationeryRequests"));
             staffSidenavItems.Add(new SidenavItem("Disbursement Lists", "/Department/DisbursementLists"));
-            staffSidenavItems.Add(new SidenavItem("Notifications", "/Notifications/ViewAll"));
+            staffSidenavItems.Add(new SidenavItem("Notifications", "/Department/Notifications"));
         }
         public ActionResult Index()
         {
@@ -37,6 +39,14 @@ namespace Team7ADProject.Controllers
             if (user == null) return RedirectToAction("Index", "Home");
             if (user.UserType != "departmentStaff" && user.UserType != "departmentHead") return RedirectToAction("Index", "Home");
             ViewData["sidenavItems"] = staffSidenavItems;
+            return View();
+        }
+        public ActionResult Notifications()
+        {
+            User user = userService.GetUserFromCookie(Request.Cookies["Team7ADProject"]);
+            if (user == null) return RedirectToAction("Index", "Home");
+            if (user.UserType != "departmentStaff" && user.UserType != "departmentHead") return RedirectToAction("Index", "Home");
+            ViewData["sidenavItems"] = user.UserType == "departmentStaff"? staffSidenavItems : headSidenavItems;
             return View();
         }
     }
---
+++ Team7ADProject/NotificationService_ForReference.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Team7ADProject.Database;
+using Team7ADProject.Models;
+
+namespace Team7ADProject
+{
+    public class NotificationService
+    {
+        //NOTE: MUST have notificationType!
+        public String NotificationSubject(String notificationType, User sender)
+        {
+            string subject = "";
+
+            switch(notificationType)
+            {
+                case "StationeryRequest_ForApproval":
+                    subject = "For Approval: Stationery Request From " + sender.Name;
+                    break;
+
+                case "StationeryRequest_Approved":
+                    subject = "Approved: Stationery Request";
+                    break;
+
+                case "StationeryRequest_Rejected":
+                    subject = "Rejected: Stationery Request";
+                    break;
+
+                case "StationeryRequest_StoreClerk":
+                    subject = "New Stationery Request From " + sender.Name;
+                    break;
+
+                case "Disbursement_Ready":
+                    subject = "Stationery Is Ready For Collection";
+                    break;
+
+                case "Disbursement_Collected":
+                    subject = "Stationery Has Been Collected";
+                    break;
+
+                case "Disbursement_NotCollected":
+                    subject = "Oh No! Not Collected: Stationery";
+                    break;
+                
+                /*case "":
+                    subject = "" + ;
+                    break;
+
+                case "":
+                    subject = "" + ;
+                    break;*/
+
+                default:
+                    subject = "";
+                    break;
+            }
+            return subject;
+        }
+
+        public String NotificationContent(String notificationType, User sender)
+        {
+            string content = "";
+
+            switch (notificationType)
+            {
+                case "StationeryRequest_ForApproval":
+                    content = "Dear Sir/Mdm,\n" + sender.Name + " has submitted a new request for stationery.";
+                    //INCLUDE: Link to Approval Page
+                    break;
+
+                case "StationeryRequest_Approved":
+                    content = "Your Stationery Request has been Approved.";
+                    //Anything else to say?
+                    break;
+
+                case "StationeryRequest_Rejected":
+                    content = "Your Stationery Request has been Rejected.";
+                    //INCLUDE: Link to view Remarks?
+                    break;
+
+                case "StationeryRequest_StoreClerk":
+                    content = "There is a new Stationery Request from " + sender.Name + ".";
+                    //INCLUDE: Link to view Request
+                    break;
+
+                case "Disbursement_Ready":
+                    content = "Your stationery is ready for collection from ";
+                    //INCLUDE: Link to view Item List; Location & Date & Time of Collection & from who
+                    break;
+
+                case "Disbursement_Collected":
+                    content = "Thank you for collecting your stationery. Here's a list of the items you've collected:\n\n";
+                    //INCLUDE: Either List or Link to List
+                    break;
+
+                case "Disbursement_NotCollected":
+                    content = "Oh dear. It seems you did not collect the stationery you requested. How about ";
+                    //URGENT: What's the Resolution???
+                    break;
+
+                /*case "":
+                    content = "" + ;
+                    break;
+
+                case "":
+                    content = "" + ;
+                    break;*/
+
+                default:
+                    content = "";
+                    break;
+            }
+            return content;
+        }
+
+
+        //Get Subject & Message from where?
+        public Notification CreateNotification(User sendingUser, String notificationType)
+        {
+            User sender = sendingUser;
+            Notification notification = new Notification(DateTime.Now.ToString("dddd-MMMM-yyyy"), sender.Name, NotificationSubject(notificationType, sender), NotificationContent(notificationType, sender));
+            return notification;
+        }
+
+        //Add New Notification to Notification Table in DB
+        public void AddNotificationToDB(Notification notification)
+        {
+            var db = new Team7ADProjectDbContext();
+            db.Notification.Add(notification);
+            db.SaveChanges();
+
+        }
+
+        //Add Pair New Notification to Recipient in Notification_Users table in DB
+        //Get the recipient from where?
+        public void SendNotificationToRecipient(Notification notification)
+        {
+            
+        }
+
+    }
+
+}
