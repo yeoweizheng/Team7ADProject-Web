b06693f3821801397990e48d94ff910d74baa792
Wei Zheng <wzyeo1990@gmail.com>
2020-01-21 09:39:02 +0800

    Separated staff & head stationery requests

--- Team7ADProject/Controllers/DepartmentController.cs
+++ Team7ADProject/Controllers/DepartmentController.cs
@@ -22,7 +22,7 @@ namespace Team7ADProject.Controllers
             userService = new UserService();
             notificationService = new NotificationService();
             staffSidenavItems = new List<SidenavItem>();
-            staffSidenavItems.Add(new SidenavItem("Stationery Requests", "/Department/StationeryRequests"));
+            staffSidenavItems.Add(new SidenavItem("Stationery Requests", "/Department/StaffStationeryRequests"));
             staffSidenavItems.Add(new SidenavItem("Disbursement Lists", "/Department/DisbursementLists"));
             staffSidenavItems.Add(new SidenavItem("Notifications", "/Department/Notifications"));
         }
@@ -30,14 +30,21 @@ namespace Team7ADProject.Controllers
         {
             User user = userService.GetUserFromCookie(Request.Cookies["Team7ADProject"]);
             if (user == null) return RedirectToAction("Index", "Home");
-            if (user.UserType != "departmentStaff" && user.UserType != "departmentHead") return RedirectToAction("Index", "Home");
-            return RedirectToAction("StationeryRequests");
+            switch (user.UserType)
+            {
+                case "departmentStaff":
+                    return RedirectToAction("StaffStationeryRequests");
+                case "departmentHead":
+                    return RedirectToAction("HeadStationeryRequests");
+                default:
+                    return RedirectToAction("Index", "Home");
+            }
         }
-        public ActionResult StationeryRequests()
+        public ActionResult StaffStationeryRequests()
         {
             User user = userService.GetUserFromCookie(Request.Cookies["Team7ADProject"]);
             if (user == null) return RedirectToAction("Index", "Home");
-            if (user.UserType != "departmentStaff" && user.UserType != "departmentHead") return RedirectToAction("Index", "Home");
+            if (user.UserType != "departmentStaff") return RedirectToAction("Index", "Home");
             ViewData["sidenavItems"] = staffSidenavItems;
             List<StationeryRequest> stationeryRequests = db.StationeryRequest.ToList();
             ViewData["stationeryRequests"] = stationeryRequests;
