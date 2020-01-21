767510ccec3e200e98945165753735b86750b38f
Wei Zheng <wzyeo1990@gmail.com>
2020-01-21 10:17:54 +0800

    Added store supervisor pages

--- Team7ADProject/Controllers/StoreController.cs
+++ Team7ADProject/Controllers/StoreController.cs
@@ -12,6 +12,7 @@ namespace Team7ADProject.Controllers
     public class StoreController : Controller
     {
         private static List<SidenavItem> clerkSideNavItems;
+        private static List<SidenavItem> supSideNavItems;
         private static Team7ADProjectDbContext db;
         private static UserService userService;
         private static StationeryService stationeryService;
@@ -28,17 +29,30 @@ namespace Team7ADProject.Controllers
             clerkSideNavItems.Add(new SidenavItem("Stationery Retrieval List", "/Store/RetrievalList"));
             clerkSideNavItems.Add(new SidenavItem("Disbursement List", "/Store/DisbursementList"));
             clerkSideNavItems.Add(new SidenavItem("Stock List", "/Store/StockList"));
-            clerkSideNavItems.Add(new SidenavItem("Adjustment Vouchers", "/Store/AdjustmentVouchers"));
+            clerkSideNavItems.Add(new SidenavItem("Adjustment Vouchers", "/Store/ClerkAdjustmentVouchers"));
             clerkSideNavItems.Add(new SidenavItem("Orders", "/Store/Orders"));
             clerkSideNavItems.Add(new SidenavItem("Notifications", "/Store/Notifications"));
+            supSideNavItems = new List<SidenavItem>();
+            supSideNavItems.Add(new SidenavItem("Stock List", "/Store/StockList"));
+            supSideNavItems.Add(new SidenavItem("Adjustment Vouchers", "/Store/SupAdjustmentVouchers"));
+            supSideNavItems.Add(new SidenavItem("Orders", "/Store/Orders"));
+            supSideNavItems.Add(new SidenavItem("Reports", "/Store/Reports"));
+            supSideNavItems.Add(new SidenavItem("Notifications", "/Store/Notifications"));
         }
 
         public ActionResult Index()
         {
             User user = userService.GetUserFromCookie(Request.Cookies["Team7ADProject"]);
             if (user == null) return RedirectToAction("Index", "Home");
-            if (user.UserType != "storeClerk" && user.UserType != "storeSupervisor") return RedirectToAction("Index", "Home");
-            return RedirectToAction("DepartmentRequests");
+            switch (user.UserType)
+            {
+                case "storeClerk":
+                    return RedirectToAction("DepartmentRequests");
+                case "storeSupervisor":
+                    return RedirectToAction("StockList");
+                default:
+                    return RedirectToAction("Index", "Home");
+            }
         }
         public ActionResult DepartmentRequests()
         {
@@ -70,7 +84,7 @@ namespace Team7ADProject.Controllers
             User user = userService.GetUserFromCookie(Request.Cookies["Team7ADProject"]);
             if (user == null) return RedirectToAction("Index", "Home");
             if (user.UserType != "storeClerk" && user.UserType != "storeSupervisor") return RedirectToAction("Index", "Home");
-            ViewData["sidenavItems"] = clerkSideNavItems;
+            ViewData["sidenavItems"] = user.UserType == "storeClerk" ? clerkSideNavItems : supSideNavItems;
             ViewData["stationeries"] = stationeryService.GetStationeries();
             return View();
         }
@@ -85,7 +99,7 @@ namespace Team7ADProject.Controllers
             return View();
         }
 
-        public ActionResult AdjustmentVouchers()
+        public ActionResult ClerkAdjustmentVouchers()
         {
             User user = userService.GetUserFromCookie(Request.Cookies["Team7ADProject"]);
             if (user == null) return RedirectToAction("Index", "Home");
