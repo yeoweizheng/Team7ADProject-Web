4e9e95e01d022812274dab26ca6379eae2f9ac2b
Wei Zheng <wzyeo1990@gmail.com>
2020-01-25 16:56:18 +0800

    Added rest controller for android

---
+++ Team7ADProject/Controllers/RestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Team7ADProject.Controllers
+{
+    public class RestController : Controller
+    {
+        public JsonResult TestConnection(string header, string body)
+        {
+            System.Diagnostics.Debug.WriteLine("msg");
+            System.Diagnostics.Debug.WriteLine(header);
+            return Json(new { result = "success" });
+        }
+    }
+}
